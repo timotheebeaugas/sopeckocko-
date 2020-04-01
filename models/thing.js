c0f3ee1794f04606ba44c75cd4ff1994c3a1ba65
Your Name <you@example.com>
2020-04-01 23:07:24 +0200

    fix routes and controllers

--- models/thing.js
+++ models/thing.js
@@ -2,18 +2,19 @@
 
 const mongoose = require('mongoose');
 
-const thingSchema = mongoose.Schema({ // +  identifiant unique créé par MongoDB ;
-  userId: { type: String, required: true }, //  identifiant unique MongoDB pour l'utilisateur qui a créé lasauce
+const thingSchema = mongoose.Schema({ 
+  _id: { type: String, required: true }, // +  identifiant unique créé par MongoDB
   name: { type: String, required: true }, // nom de la sauce 
   manufacturer: { type: String, required: true }, // fabricant de la sauce 
   description: { type: String, required: true }, // description de la sauce 
-  mainingredient: { type: String, required: true }, // principal ingrédient dans la sauce 
-  imageUrl: { type: String, required: true }, // string de l'image de la sauce téléchargée par l'utilisateur 
   heat: { type: Number, required: true }, // nombre entre 1 et 10 décrivant la sauce
   likes: { type: Number, required: true }, // nombre d'utilisateurs qui aiment la sauce
   dislikes: { type: Number, required: true }, //  nombre d'utilisateurs qui n'aiment pas la sauce
-  usersliked: { type: String, required: true }, //  tableau d'identifiants d'utilisateurs ayant aimé la sauce 
-  usersdisliked: { type: String, required: true }, //  tableau d'identifiants d'utilisateurs n'ayant pas aiméla sauce 
+  imageUrl: { type: String, required: true }, // string de l'image de la sauce téléchargée par l'utilisateur 
+  mainPepper: { type: String, required: true }, // principal ingrédient dans la sauce 
+  usersLiked: { type: String, required: true }, //  tableau d'identifiants d'utilisateurs ayant aimé la sauce 
+  usersDisliked: { type: String, required: true }, //  tableau d'identifiants d'utilisateurs n'ayant pas aiméla sauce 
+  userId: { type: String, required: true }, //  identifiant unique MongoDB pour l'utilisateur qui a créé lasauce
 });
 
 module.exports = mongoose.model('thing', thingSchema);
--- routes/stuff.js
+++ routes/stuff.js
@@ -13,7 +13,7 @@ const multer = require('../middleware/multer-config');
 
 // requête POST
 router.post('/', auth, multer, stuffCtrl.creatThing);
-router.post('/:id/like', auth, multer, stuffCtrl.creatThing); // Thing à créer pour gérer les likes
+router.post('/:id/like', auth, stuffCtrl.creatThing); // Thing à créer pour gérer les likes
   
 // Route PUT pour modifier l'objet en fonction de son ID
 router.put('/:id', auth, multer, stuffCtrl.modifyThing);
